a7964aeafe247e7a536db354ba22bb62551357d8
IamFennics <andrej.darmenko@gmail.com>
2020-08-15 17:09:36 +0300

    Отделяем DLL от UI в файлах Dialogs.jsx, Profile.jsx

--- src/componets/pages/Dialogs/Dialogs.jsx
+++ src/componets/pages/Dialogs/Dialogs.jsx
@@ -5,22 +5,43 @@ import UserDialog from './UserDialog/UserDialog';
 import MessageItem from './Messages/MessageItem';
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    {id: 0, name: 'Nick'},
+    {id: 1, name: 'Mishele'},
+    {id: 2, name: 'Victor'},
+    {id: 3, name: 'Andrey'},
+    {id: 4, name: 'John'},
+    {id: 5, name: 'Tom'},
+    {id: 6, name: 'Anna'}
+  ];
+
+  let messageData = [
+    {id: 0, title: 'Nick`s message 1', mess: 'Lorem 1 ipsum dolor sit amet consectetur adipisicing elit. Accusantium...'},
+    {id: 1, title: 'Anna message 1',   mess: 'Lorem 2 ipsum dolor sit amet consectetur adipisicing elit. Accusantium...'},
+    {id: 2, title: 'Anna message 2',   mess: 'Lorem 3 ipsum dolor sit amet consectetur adipisicing elit. Accusantium...'},
+    {id: 3, title: 'John message 1',   mess: 'Lorem 4 ipsum dolor sit amet consectetur adipisicing elit. Accusantium...'},
+    {id: 4, title: 'Nick`s message 2', mess: 'Lorem 5 ipsum dolor sit amet consectetur adipisicing elit. Accusantium...'}
+  ];
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__users}>
         <h1>Dialogs</h1>
 
-        <UserDialog userName="Nick" id="0" />
-        <UserDialog userName="Mishele" id="1" />
-        <UserDialog userName="Victor" id="2" />
-        <UserDialog userName="Andrey" id="3" />
-        <UserDialog userName="John" id="4" />
-        <UserDialog userName="Tom" id="5" />
-        <UserDialog userName="Anna" id="6" />
+        <UserDialog userName={dialogsData[0].name} id={dialogsData[0].id} />
+        <UserDialog userName={dialogsData[1].name} id={dialogsData[1].id} />
+        <UserDialog userName={dialogsData[2].name} id={dialogsData[2].id} />
+        <UserDialog userName={dialogsData[3].name} id={dialogsData[3].id} />
+        <UserDialog userName={dialogsData[4].name} id={dialogsData[4].id} />
+        <UserDialog userName={dialogsData[5].name} id={dialogsData[5].id} />
+        <UserDialog userName={dialogsData[6].name} id={dialogsData[6].id} />
       </div>
 
       <div className={s.dialogs__messages}>
-        <MessageItem title='Nick`s message 1' message=' Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, rem modi! Tempore delectus explicabo possimus earum esse, iste asperiores rerum assumenda molestias, sunt perspiciatis numquam quod eos.' />
+        <MessageItem id={messageData[0].id} title={messageData[0].title} message={messageData[0].mess} />
+        <MessageItem id={messageData[1].id} title={messageData[1].title} message={messageData[1].mess} />
+        <MessageItem id={messageData[2].id} title={messageData[2].title} message={messageData[2].mess} />
+        <MessageItem id={messageData[3].id} title={messageData[3].title} message={messageData[3].mess} />
+        <MessageItem id={messageData[4].id} title={messageData[4].title} message={messageData[4].mess} />
       </div>
     </div>
   );
--- src/componets/pages/Profile/MyPosts/MyPosts.jsx
+++ src/componets/pages/Profile/MyPosts/MyPosts.jsx
@@ -3,15 +3,23 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  let postsData = [
+    {id: 0, title: 'Post 1', like: 23},
+    {id: 1, title: 'Post 2', like: 432},
+    {id: 2, title: 'Post 3', like: 54},
+    {id: 3, title: 'Post 4', like: 0},
+    {id: 4, title: 'Post 5', like: 34567},
+  ];
+
   return (
     <div>
       <h2>My posts</h2>
       <div className={s.posts}>
-        <Post title="Post 1" like="23" />
-        <Post title="Post 2" like="432" />
-        <Post title="Post 3" like="54" />
-        <Post title="Post 4" like="0" />
-        <Post title="Post 5" like="34567" />
+        <Post title={postsData[0].title} like={postsData[0].like} />
+        <Post title={postsData[1].title} like={postsData[1].like} />
+        <Post title={postsData[2].title} like={postsData[2].like} />
+        <Post title={postsData[3].title} like={postsData[3].like} />
+        <Post title={postsData[4].title} like={postsData[4].like} />
       </div>
     </div>
   );
