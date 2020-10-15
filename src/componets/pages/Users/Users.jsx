f332de169099d18912d9e57ca2d26b9102339f45
IamFennics <andrej.darmenko@gmail.com>
2020-10-15 20:52:15 +0300

    Заменяем презентационную компоненту Users на классовую

--- src/componets/pages/Users/Users.jsx
+++ src/componets/pages/Users/Users.jsx
@@ -3,43 +3,45 @@ import React from 'react';
 import s from './Users.module.css';
 import userPhoto from '../../../../src/assets/img/woman1.png';
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    if(props.users.length === 0){
+    constructor(props) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then( response => 
-                { props.setUsers( response.data.items ) }     
-            );    
+            .then(response => { this.props.setUsers(response.data.items) }
+            );
     }
-    
 
-    return (
-        <div className={s.users}>
-            <h1>Users</h1>
-            <hr />
-            {
-                props.users.map(u =>
-                    <div className={s.userGrid} key={u.id}>
-                        <div className={s.userGrid_left}>
-                            <img src={ u.photos.small != null? u.photos.small : userPhoto } alt="avatar" />
-                            {u.followed
-                                ? <button className={s.follow} onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                : <button className={s.follow} onClick={() => { props.follow(u.id) }}>Follow</button>
-                            }
+
+    render() {
+        return (
+            <div className={s.users}>
+                <h1>Users</h1>
+                <hr />
+                {
+                    this.props.users.map(u =>
+                        <div className={s.userGrid} key={u.id}>
+                            <div className={s.userGrid_left}>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="avatar" />
+                                {u.followed
+                                    ? <button className={s.follow} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                    : <button className={s.follow} onClick={() => { this.props.follow(u.id) }}>Follow</button>
+                                }
+                            </div>
+
+                            <div className={s.userGrid_right}>
+                                <span> {u.name} </span>
+                                <span> {"u.location.country"} </span>
+                                <span> {u.status} </span>
+                                <span> {"u.location.city"} </span>
+                            </div>
                         </div>
+                    )
+                }
 
-                        <div className={s.userGrid_right}>
-                            <span> {u.name} </span>
-                            <span> {"u.location.country"} </span>
-                            <span> {u.status} </span>
-                            <span> {"u.location.city"} </span>
-                        </div>
-                    </div>
-                )
-            }
-
-        </div>
-    );
+            </div>
+        );
+    }
 }
 
 export default Users;
