6485c046069af3600429f68349c6f63e1417f563
IamFennics <andrej.darmenko@gmail.com>
2020-10-18 19:07:35 +0300

    Реорганизуем классовую компоненту в контейнерную и презентационную

--- src/componets/pages/Users/Users.jsx
+++ src/componets/pages/Users/Users.jsx
@@ -1,65 +1,53 @@
-import * as axios from 'axios';
 import React from 'react';
 import s from './Users.module.css';
 import userPhoto from '../../../../src/assets/img/woman1.png';
 
-class Users extends React.Component {
+const Users = (props) => {
 
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => { this.props.setUsers(response.data.items) }
-        );
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= 10; i++) {
+        pages.push(i);
     }
 
-    render(props) {
+    return (
+        <div className={s.users}>
+            <h1>Users</h1>
+            <hr />
 
-        let pagesCount = Math.ceil( this.props.totalUsersCount / this.props.pageSize );
+            <div className={s.pagination}>
+                {pages.map(p =>
+                    <span key={p.id} className={props.currentPage === p && s.active}
+                        onClick={() => { props.onPageChanged(p) }} >{p}</span>
 
-        let pages = [];
-        for(let i = 1; i <= pagesCount; i++){
-            pages.push(i);
-        }
-
-        return (
-            <div className={s.users}>
-                <h1>Users</h1>
-                <hr />
-
-                <div className={s.pagination}>
-                    { pages.map( p => 
-                            <span className={ this.props.currentPage === p && s.active } 
-                            onClick={ () => { this.props.setCurrentPage(p) } } >{p}</span>
-                           
-                    ) }
-                </div>
-                <hr />
-
-                {
-                    this.props.users.map(u =>
-                        <div className={s.userGrid} key={u.id}>
-                            <div className={s.userGrid_left}>
-                                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="avatar" />
-                                {u.followed
-                                    ? <button className={s.follow} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
-                                    : <button className={s.follow} onClick={() => { this.props.follow(u.id) }}>Follow</button>
-                                }
-                            </div>
-
-                            <div className={s.userGrid_right}>
-                                <span> {u.name} </span>
-                                <span> {"u.location.country"} </span>
-                                <span> {u.status} </span>
-                                <span> {"u.location.city"} </span>
-                            </div>
-                        </div>
-                    )
-                }
-
+                )}
             </div>
-        );
-    }
+            <hr />
 
+            {
+                props.users.map(u =>
+                    <div className={s.userGrid} key={u.id}>
+                        <div className={s.userGrid_left}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="avatar" />
+                            {u.followed
+                                ? <button className={s.follow} onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
+                                : <button className={s.follow} onClick={() => { props.follow(u.id) }}>Follow</button>
+                            }
+                        </div>
 
+                        <div className={s.userGrid_right}>
+                            <span> {u.name} </span>
+                            <span> {"u.location.country"} </span>
+                            <span> {u.status} </span>
+                            <span> {"u.location.city"} </span>
+                        </div>
+                    </div>
+                )
+            }
+
+        </div>
+    );
 }
 
 export default Users;
--- src/redux/usersReduser.js
+++ src/redux/usersReduser.js
@@ -2,11 +2,12 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     users: [],
-    pageSize: 5,
-    totalUsersCount: 20,
+    pageSize: 7,
+    totalUsersCount: 0,
     currentPage: 1
 };
 
@@ -37,9 +38,13 @@ const usersReduser = (state = initialState, action) => {
         case SET_USERS:
             return {
                 ...state,
-                users: [ ...state.users, ...action.users ]
+                users: action.users // Перезатирка users-ов
+                //users: [ ...state.users, ...action.users ] Добавление user-ов в конец сейта
             }
-           
+        case SET_TOTAL_USERS_COUNT:
+            return {
+                ...state, totalUsersCount: action.count
+            }
         case SET_CURRENT_PAGE:
             return {
                 ...state, currentPage: action.currentPage
@@ -61,6 +66,7 @@ export const unfollowAC = (userId) => {
 export const setUsersAC = (users) => {
     return { type: SET_USERS, users }
 }
+export const setTotalUsersCountAC = (totalCount) => ({  type: SET_TOTAL_USERS_COUNT, count: totalCount })
 export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
 
 export default usersReduser;
