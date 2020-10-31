4682fbc9ff8733f6498ffb450a03544afa1c4922
IamFennics <andrej.darmenko@gmail.com>
2020-10-31 22:08:38 +0200

    Updates

---
+++ precache-manifest.94949c59c774a1cd359df094bd749e9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5306247c7e37e3e1aff4953414a441d4",
+    "url": "/myIt-camasutra/index.html"
+  },
+  {
+    "revision": "9fe3ec957a8066c4aced",
+    "url": "/myIt-camasutra/static/css/main.cfb65a78.chunk.css"
+  },
+  {
+    "revision": "ade1be7ad4d15540528f",
+    "url": "/myIt-camasutra/static/js/2.205e1616.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/myIt-camasutra/static/js/2.205e1616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe3ec957a8066c4aced",
+    "url": "/myIt-camasutra/static/js/main.b6cc556e.chunk.js"
+  },
+  {
+    "revision": "dbddb7123b360380aba5",
+    "url": "/myIt-camasutra/static/js/runtime-main.09615ab8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myIt-camasutra/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dddf8d504ed0d3abf5e7a6f83cf6e293",
+    "url": "/myIt-camasutra/static/media/preloader.dddf8d50.gif"
+  }
+]);
